6b789d7c24bb386ffb07a2807b1ad0f9563a47a2
larsanik <larsanik@gmail.com>
2022-01-06 23:17:03 +0300

    Commit_06.01.2022_23:17:03,95

--- polls/views.py
+++ polls/views.py
@@ -3,8 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 from polls.ant10 import AHT10
-m = AHT10(1)
 
 def index(request):
+    m = AHT10(1)
     data = m.getData()
     return HttpResponse("Hello, world. You're at the polls index." + "Temperature: %.2f C" % data[0] + "Humidity: %.2f " % data[1] + "%")
