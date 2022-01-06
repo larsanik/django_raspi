6f9f761b20e46c76ec3763996f65262511641022
larsanik <larsanik@gmail.com>
2022-01-06 23:05:38 +0300

    Commit_06.01.2022_23:05:38,94

--- polls/views.py
+++ polls/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
-from ant10 import AHT10
+from polls.ant10 import AHT10
 m = AHT10(1)
 
 def index(request):
