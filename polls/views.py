2fac8a799a61a360449ac4ae0d7c081246feb59c
larsanik <larsanik@gmail.com>
2022-01-06 23:24:33 +0300

    Commit_06.01.2022_23:24:33,45

--- polls/views.py
+++ polls/views.py
@@ -3,8 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 from polls.ant10 import AHT10
+from polls.bmp180_a import readBmp180
 
 def index(request):
     m = AHT10(1)
     data = m.getData()
-    return HttpResponse("Temperature: %.2f C" % data[0] + "\n" "Humidity: %.2f " % data[1] + "%")
+    temp, pressure, altitude = readBmp180()
+    return HttpResponse("Temperature: %.2f C " % data[0] + "Humidity: %.2f " % data[1] + "% " + "Pressure:    %.2f mmHg" % (pressure / (100.0 * 1.333)))
