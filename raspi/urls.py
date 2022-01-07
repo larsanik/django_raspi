9c38b405f15a0596e82d5a844f2202010e7bdba6
larsanik <larsanik@gmail.com>
2022-01-08 02:10:02 +0300

    Commit_08.01.2022_ 2:10:02,78

--- raspi/urls.py
+++ raspi/urls.py
@@ -1,4 +1,4 @@
-"""locallibrary URL Configuration
+"""raspi URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -14,11 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
-from locallibrary.polls import views
+from django.urls import path, include
+from raspi.meteo import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    #path('meteo/', include('raspi.meteo.urls')),
     path('', views.index, name='index'),
 ]
