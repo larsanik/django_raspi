ae70b6075a51fbae422b5c176abd0bbaca31992a
larsanik <larsanik@gmail.com>
2022-01-08 00:01:34 +0300

    Commit_08.01.2022_ 0:01:34,09

--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from polls import views
+from locallibrary.polls import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
