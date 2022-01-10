f446d45e9bede47e616ef3df03a3a7336e040382
larsanik <larsanik@gmail.com>
2022-01-10 09:26:53 +0300

    frontEnd_1

--- raspi/meteo/views.py
+++ raspi/meteo/views.py
@@ -5,16 +5,16 @@
 from django.shortcuts import render
 
 
-from raspi.meteo.ant10 import AHT10
-from raspi.meteo.bmp180_a import readBmp180
+# from raspi.meteo.ant10 import AHT10
+# from raspi.meteo.bmp180_a import readBmp180
 
 def index(request):
-    m = AHT10(1)
-    data = m.getData()
-    temperature = data[0]
-    humidity = data[1]
-    temp, pressure, altitude = readBmp180()
-    press = pressure / (100.0 * 1.333)
+    # m = AHT10(1)
+    # data = m.getData()
+    temperature = 23.234234 # data[0]
+    humidity = 75.223232 # data[1]
+    # temp, pressure, altitude = readBmp180()
+    press = 758.343434 # pressure / (100.0 * 1.333)
     # return HttpResponse("Temperature: %.2f C " % temperature + "Humidity: %.2f " % humidity + "% " + "Pressure:
     # %.2f mmHg" % press)
     return render(
