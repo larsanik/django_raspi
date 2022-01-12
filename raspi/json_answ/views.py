c8d9e52b5d7e7cf6e9158ddc69f2d40f69d767eb
larsanik <larsanik@gmail.com>
2022-01-12 16:48:13 +0300

    json,web random

--- raspi/json_answ/views.py
+++ raspi/json_answ/views.py
@@ -2,18 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import JsonResponse
-
-# from raspi.meteo.ant10 import AHT10
-# from raspi.meteo.bmp180_a import readBmp180
+from raspi.input.input import sensorPolling
 
 def profile(request):
-    # m = AHT10(1)
-    # data = m.getData()
-    temperature = 23.234234 # data[0]
-    humidity = 75.223232 # data[1]
-    # temp, pressure, altitude = readBmp180()
-    press = 758.343434 # pressure / (100.0 * 1.333)
-
+    temperature, humidity, press = sensorPolling()
     data = {
         'temperature': temperature,
         'humidity': humidity,
--- raspi/meteo/views.py
+++ raspi/meteo/views.py
@@ -3,20 +3,11 @@
 # Create your views here.
 # from django.http import HttpResponse
 from django.shortcuts import render
+from raspi.input.input import sensorPolling
 
-
-# from raspi.meteo.ant10 import AHT10
-# from raspi.meteo.bmp180_a import readBmp180
 
 def index(request):
-    # m = AHT10(1)
-    # data = m.getData()
-    temperature = 23.234234 # data[0]
-    humidity = 75.223232 # data[1]
-    # temp, pressure, altitude = readBmp180()
-    press = 758.343434 # pressure / (100.0 * 1.333)
-    # return HttpResponse("Temperature: %.2f C " % temperature + "Humidity: %.2f " % humidity + "% " + "Pressure:
-    # %.2f mmHg" % press)
+    temperature, humidity, press = sensorPolling()
     return render(
         request,
         'index.html',
